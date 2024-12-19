9923f37c904aa501ec82b623981ec8818e6467b2
Eokji coding <byunguk98@naver.com>
2024-12-19 17:58:41 +0900

    feat : JSX 연습
    {/* JSX Practice */} jsx 주석 처리 방법
    
    JSX에서 꼭 지켜야 할 것은, 모든 태그를 닫아줘야 한다는 것이다. 이미지 태그나 줄바꿈 태그처럼 단독으로 쓰이는 태그들의 경우도, 반드시 슬래시를 넣어 닫아줘야 한다.
    
    JSX는 HTML처럼 보이지만, 자바스크립트로 컴파일되는 문서라는 점을 기억해야 한다.
    
    JSX 형태로 작성된 요소들이 각각 상수로 저장되는 것을 볼 수 있다. 즉, 자바스크립트에서 사용될 수 있는 어떤 데이터로 변환된다는 의미다. 콘솔을 찍어보면, JSX 요소들이 자바스크립트 객체로 만들어짐을 알 수 있다. 이들은 가상 DOM을 구성하는 기본 단위가 된다.
    
    이처럼 JSX 요소들은 자바스크립트 객체로 변환될 대상이므로 HTML에 비해 엄격한 문법 준수가 요구된다.
    
    <중괄호 표현식>
    중괄호 표현식이란 말 그대로 중괄호 사이에 표현식을 넣은거다. 여기서 표현식은 어떤 값을 반환하는, 즉 다른 어떤 값으로 대체되어 작성될 수 있는 코드 조각을 말한다. 위의 "JavaScript"도 문자열로 대체될 수 있기 때문에 language 상수도 '표현식' 인 것이다. 아무튼 JSX에서 중괄호 표현식은 일정 종류의 값을 반환하는 코드 조각들을 넣어서 화면에 렌더링하는데 사용된다.
    
    주의) 원시 자료형 중에 중괄호 표현식에서 렌더링될 수 있는 것은 문자열과 숫자다.

--- yalco-practice/src/App.jsx
+++ yalco-practice/src/App.jsx
@@ -1,19 +1,66 @@
-import Child from "./Child";
-import { ClassComp, FunctionComp, ArrowFunctionComp } from "./Components";
+const element1 = <h2>Hello, World!</h2>
+const element2 = (
+  <ul>
+    <li>A</li>
+    <li>B</li>
+    <li>C</li>
+  </ul>
+)
 
-function Hello() {
-  return <h1>Hello!</h1>
+console.log(element1);
+console.log(element2);
+
+const language = "JavaScript";
+
+//  JSX 작성 연습
+function BasicExpressions() {
+  const name = 'Park';
+  const age = 25;
+  const isAdmin = true;
+
+  return (
+    <div>
+      <p>Name : {name}</p>
+      <p>Age next year: {age + 1}</p>
+      <p>{name + "'s Profile"}</p>
+      <p>{`${name} is ${age} years old.`}</p>
+      <p>Admin status : {String(isAdmin)}</p>
+    </div>
+  );
 }
+
+function ObjectArrayExpressions() {
+  const user = {
+    name : "Karina",
+    email : "karina@aespa.com"
+  };
+  const colors = ["red", "blue", "green"];
+  const numbers = [1, 2, 3, 4, 5];
+
+  return (
+    <div>
+      <p>User : {user.name} {user.email}</p>
+      <p>First color : {colors[0]}</p>
+      <p>Color count : {colors.length}</p>
+
+      <p>Doubleds : {
+          numbers.map((num) => (num * 2)).join(", ")
+        }</p>
+
+        <p>Evens : {
+        numbers.filter((num) => (num & 2 === 0)).join(", ")
+        }</p>
+    </div>
+  );
+} 
 
 function App() {
   return (
     <>
-      <Hello />
-      <h2>World</h2>
-      <Child />
-      <ClassComp />
-      <FunctionComp />
-      <ArrowFunctionComp />
+      <h1>JSX</h1>
+      {language}
+      <BasicExpressions />
+      <ObjectArrayExpressions />
     </>
   );
 }
