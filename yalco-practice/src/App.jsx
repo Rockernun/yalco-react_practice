3d0f696dba9412f74a70956c05459305f2dff9b0
Eokji coding <byunguk98@naver.com>
2024-12-21 20:37:16 +0900

    feat : 상태 끌어올리기 예제

--- yalco-practice/src/App.jsx
+++ yalco-practice/src/App.jsx
@@ -1,68 +1,34 @@
-const element1 = <h2>Hello, World!</h2>
-const element2 = (
-  <ul>
-    <li>A</li>
-    <li>B</li>
-    <li>C</li>
-  </ul>
-)
+import './App.css'
+import { useState } from 'react'
+import TempInput from './TempInput'
+import UnitSelector from './UnitSelector'
 
-console.log(element1);
-console.log(element2);
+const App = () => {
+  const [temperature, setTemperature] = useState("")
+  const [unit, setUnit] = useState("Celsius")
 
-const language = "JavaScript";
-
-//  JSX 작성 연습
-function BasicExpressions() {
-  const name = 'Park';
-  const age = 25;
-  const isAdmin = true;
+  const convertedTemp = unit === "Celsius"
+    ? (temperature * 9/5 + 32).toFixed(1)
+    : ((temperature - 32) * 5/9).toFixed(1)
 
   return (
     <div>
-      <p>Name : {name}</p>
-      <p>Age next year: {age + 1}</p>
-      <p>{name + "'s Profile"}</p>
-      <p>{`${name} is ${age} years old.`}</p>
-      <p>Admin status : {String(isAdmin)}</p>
+      <h2>Temperature Converter</h2>
+      <p>
+        Converted: {temperature ? convertedTemp : "--"} 
+        {unit === "Celsius" ? "°F" : "°C"}
+      </p>
+      <TempInput
+        value={temperature}
+        unit={unit}
+        onChange={setTemperature}
+      />
+      <UnitSelector
+        unit={unit}
+        onUnitChange={setUnit}
+      />
     </div>
-  );
+  )
 }
 
-function ObjectArrayExpressions() {
-  const user = {
-    name : "Karina",
-    email : "karina@aespa.com"
-  };
-  const colors = ["red", "blue", "green"];
-  const numbers = [1, 2, 3, 4, 5];
-
-  return (
-    <div>
-      <p>User : {user.name} {user.email}</p>
-      <p>First color : {colors[0]}</p>
-      <p>Color count : {colors.length}</p>
-
-      <p>Doubleds : {
-          numbers.map((num) => (num * 2)).join(", ")
-        }</p>
-
-        <p>Evens : {
-        numbers.filter((num) => (num & 2 === 0)).join(", ")
-        }</p>
-    </div>
-  );
-} 
-
-function App() {
-  return (
-    <>
-      <h1>JSX</h1>
-      {language}
-      <BasicExpressions />
-      <ObjectArrayExpressions />
-    </>
-  );
-}
-
-export default App;
+export default App
