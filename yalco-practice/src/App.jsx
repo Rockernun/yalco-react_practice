6185cbec01d726ecc46a7aa13eddb0f788834569
Eokji coding <byunguk98@naver.com>
2024-12-29 17:35:35 +0900

    feat : useRef 사용 예제 1

--- yalco-practice/src/App.jsx
+++ yalco-practice/src/App.jsx
@@ -1,32 +1,12 @@
 import './App.css'
-import React, { useReducer } from 'react'
-import { userReducer, initialState }
- from './reducers/userReducer'
 
-function App() {
-  const [state, dispatch]
-   = useReducer(userReducer, initialState)
+import Counter from './Counter'
 
-  return (
-    <div>
-      <input
-        type="text" placeholder="Enter name"
-        value={state.name}
-        onChange={(e) => dispatch({ 
-          type: 'SET_NAME',  payload: e.target.value })}
-      />
-      <input
-        type="number" placeholder="Enter birth year"
-        value={state.year}
-        onChange={(e) => dispatch({ 
-          type: 'SET_YEAR', payload: e.target.value })}
-      />
-      {state.warning
-       && <p style={{ color: 'red' }}>{state.warning}</p>}
-      <p>Name: {state.name}</p>
-      <p>Year: {state.year}</p>
-    </div>
-  )
-}
+const App = () => (
+  <>
+    <Counter/>
+    <Counter />
+  </>
+)
 
 export default App
